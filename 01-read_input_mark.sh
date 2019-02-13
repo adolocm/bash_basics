a7e4c51e73585c3986a6abdebdde14fe0c9027e6
Mark-Stiller <mstiller1@ggc.edu>
2019-02-13 17:35:11 -0500

    Copied the 01-read_input_ADM.sh to 01-read_input_mark.sh and altered as defined in class.

---
+++ 01-read_input_mark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+read name
+echo "Welcome, $name"
+
+echo "Would you like to be called something else?"
+read nickname
+echo "Alright. Have a nice day, $nickname"
